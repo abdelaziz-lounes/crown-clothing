77034dc73668008a74e8a428c7201f3d47e193c9
Abdelaziz <80010878+alounas@users.noreply.github.com>
2022-05-03 18:50:07 +0100

    breaking our homepage into components and finishing styling our home page

---
+++ src/components/menu-item/menu-item.component.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import './menu-item.styles.scss';
+
+const MenuItem = ({title,imageUrl,size}) => (
+    <div className={`menu-item ${size}`}>
+        <div style={{ backgroundImage: `url(${imageUrl})`}} className='background-image'></div>
+        <div className="content">
+            <h1 className="title">{title}</h1>
+            <span className="subtitle">shop now</span>
+        </div>
+    </div>
+);
+export default MenuItem;
