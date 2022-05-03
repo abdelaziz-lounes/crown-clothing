58478a79be0acf01198357c1102d489560cefd89
Abdelaziz <80010878+alounas@users.noreply.github.com>
2022-05-03 18:52:35 +0100

    changing title to upercase

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -5,7 +5,7 @@ const MenuItem = ({title,imageUrl,size}) => (
     <div className={`menu-item ${size}`}>
         <div style={{ backgroundImage: `url(${imageUrl})`}} className='background-image'></div>
         <div className="content">
-            <h1 className="title">{title}</h1>
+            <h1 className="title">{title.toUperCase()}</h1>
             <span className="subtitle">shop now</span>
         </div>
     </div>
